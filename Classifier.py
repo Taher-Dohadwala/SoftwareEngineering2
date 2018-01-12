0e1cc5e75003183b45a6c3ca5a180b8b8339332d
Taher Dohadwala <tkdohadwala@stu.naperville203.org>
2018-01-12 12:23:06 -0600

    bug fixing-Classifier
    I will definetly finish over the weekend and make it pretty

--- Classifier.py
+++ Classifier.py
@@ -3,48 +3,49 @@ from multivariabledual import Dual
 class linear_classifier:
 
     def __init__(self):
-        self.m = 0
+        self.m = 1
         self.b = 0
         self.grad_hist = []
         
-        
 
 
-    # parameters such as choosing loss function and optimizer
-    def compile(self,loss,optimizer, lr=0.001):
-        self.lr = lr
-        pass
-
-    # trains data based on loss function and optimizer
-    def fit(self,x_train,y_train, epochs=10):
-        self.pm = Dual(x=1)
-        self.pb = Dual(x=1)
-
-        for i in range(epochs):
-            o = 0
-
-            for s in range(len(x_train)):
-                o += loss(y_train[s],f(x_train[s]))
-
-            self.pm.x -= (o[self.pm]/len(x_train))*self.lr
-            self.pb.x -= (o[self.pb]/len(x_train))*self.lr
-            print ("m:",self.pm.x + " : "+ "b:",self.pb.x)
-    
-
-    def predict(self):
-        pass
-
-    
-    #support functions that does all the calculations
+     #support functions that does all the calculations
     def f(self,X):
         self.x = X
-        self.y = Y
         # y = mx+b
         return self.m*self.x + self.b
 
     def loss(self,y,y1):
         return (y1-y)**2
 
+
+
+    # parameters such as choosing loss function and optimizer
+    def compile(self,loss,optimizer, lr=0.001):
+        self.lr = lr
+        
+
+    # trains data based on loss function and optimizer
+    def fit(self,x_train,y_train, epochs=10):
+        self.y = y_train
+        self.pm = Dual(x=1)
+        self.pb = Dual(x=1)
+        
+
+        for i in range(epochs):
+            o = 0
+
+            for s in range(len(x_train)):
+                o += self.loss(y_train[s],self.f(x_train[s]))
+
+            print (self.pm)
+            self.pm.x -= (o[self.pm]/len(x_train))*self.lr
+            self.pb.x -= (o[self.pb]/len(x_train))*self.lr
+            print ("m:",str(self.pm.x) + " : "+ "b:",str(self.pb.x))
+    
+
+    def predict(self):
+        pass
 
         
         
--- check.py
+++ check.py
@@ -1,7 +1,20 @@
 from Classifier import linear_classifier
 
-X = [x for x in range(10)]
+X = [x for x in range(5)]
 
-Y = []
+Y = [5,10,15,20,25]
+
+
+print (str(len(X)), " : "+ str(len(Y)))
+test = linear_classifier()
+test.compile("squared","grad",lr=0.001)
+
+#test.compile(loss="something",optimizer="grad",lr=0.001)
+
+test.fit(X,Y,epochs=20)
+
+
+
+
 
 
